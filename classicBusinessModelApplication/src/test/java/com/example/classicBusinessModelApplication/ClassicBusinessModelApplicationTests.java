9c7c21a514df9de6d0193b22083423b4b50a9ad6
Keerthesha <keertheshas6@gmail.com>
2026-04-13 14:14:59 +0530

    Initial commit

---
+++ classicBusinessModelApplication/src/test/java/com/example/classicBusinessModelApplication/ClassicBusinessModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classicBusinessModelApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicBusinessModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
